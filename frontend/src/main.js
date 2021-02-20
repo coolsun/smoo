5f5c2702f2a09711c2b5dc1cdb10da214ce178d2
sunb0930 <ben@trillioninnovations.com>
2021-02-20 10:55:24 -0800

    fix popup issue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import '../build/css/mdb.css';
+
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbvue/lib/css/mdb.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+// not sure if we need mdbvue.css
+//import 'mdbvue/lib/mdbvue.css'
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 import router from './router';
-import Vue2TouchEvents from 'vue2-touch-events';
-import LoadScript from 'vue-plugin-load-script';
+//import Vue2TouchEvents from 'vue2-touch-events';
+//import LoadScript from 'vue-plugin-load-script';
 import i18n from './lang/language';
 import axios from 'axios';
 
-Vue.use(Vue2TouchEvents);
-Vue.use(LoadScript);
+//Vue.use(Vue2TouchEvents);
+//Vue.use(LoadScript);
 Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false;
