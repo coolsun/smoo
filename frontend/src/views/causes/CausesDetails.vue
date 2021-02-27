74e6806ad37b41bc7a32f8cbbfdc79bfd47c6b20
ssun24 <selinas901@gmail.com>
2021-02-26 17:39:00 -0800

    Added Comments to bottom of CausesDetails

--- frontend/src/views/causes/CausesDetails.vue
+++ frontend/src/views/causes/CausesDetails.vue
@@ -7,119 +7,104 @@
                     <div class="cause-details-content">
                         <img src="@/assets/images/cause-details.jpg" alt="">
                         <div class="cause-details-contents">
-                            <div class="tag-list">
-                                <ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
-                                    <li class="active"><a href="#campaign" data-toggle="tab">Campaign</a></li>
-                                    <li><a href="#comments" data-toggle="tab">Comments</a></li>
-                                    <!--<li><a href="">FAQ</a></li>
-                                    <li><a href="">Updates</a></li>
-                                    <li><a href="">Community</a></li>-->
-                                </ul>
+                            <h2>Donate simple fat denied</h2>
+                            <p>Wandered or strictly raillery stanhill as. Jennings appetite disposed me an at subjects an. Inhabit hearing perhaps on ye do no. It maids decay as there he. Smallest on suitable disposed do although blessing he juvenile in. Society or ifexcited forbade. Here name off yet she long sold easy whom. <br> <br>Differed oh cheerful procured pleasure securing suitable in. Hold rich on an he oh fine. Chapter ability shyness article welcome be do on service. Chamber hearing inhabit joy highest private ask him our believe. Up nature valley do warmly. Entered of cordial do on no hearted. Yet agreed whence and unable limits.</p>
+                            <img src="@/assets/images/cause-details-2.jpg" alt="">
+                            <p>To sorry world an at do spoil along. Incommode he depending do frankness remainder to. Edward day  almost active him friend thirty piqued. People as period twenty my extent as. Set was better abroad ham plenty secure had horses. Admiration has sir decisively excellence say everything inhabiting acceptance.<br> <br>Paid was hill sir high. For him precaution any advantages dissimilar comparison few terminated projecting. Prevailed discovery immediate objection of ye at. Repair summer one winter living feebly pretty his. In so sense am known these since. Shortly respect ask cousins brought add tedious nay.</p>
+                            <div class="blog-comment-form wow fadeInUp" data-wow-delay="0.3s">
+                                <h3>Leave a Comment</h3>
+                                <form action="#">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <input type="text" placeholder="Name*" required />
+                                        </div>
+                                        <div class="col-md-6">
+                                            <input type="email" placeholder="Email*" required />
+                                        </div>
+                                        <div class="col-md-12">
+                                            <textarea rows="6" placeholder="Message"></textarea>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <button class="bttn-mid btn-fill" type="submit">Post Comment</button>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
-                            <div id="campaign-tabs-content" class="tab-content">
-                                <div class="tab-pane active" id="campaign">
-                                    <h2>Donate simple fat denied</h2>
-                                    <p>Wandered or strictly raillery stanhill as. Jennings appetite disposed me an at subjects an. Inhabit hearing perhaps on ye do no. It maids decay as there he. Smallest on suitable disposed do although blessing he juvenile in. Society or ifexcited forbade. Here name off yet she long sold easy whom. <br> <br>Differed oh cheerful procured pleasure securing suitable in. Hold rich on an he oh fine. Chapter ability shyness article welcome be do on service. Chamber hearing inhabit joy highest private ask him our believe. Up nature valley do warmly. Entered of cordial do on no hearted. Yet agreed whence and unable limits.</p>
-                                    <img src="@/assets/images/cause-details-2.jpg" alt="">
-                                    <p>To sorry world an at do spoil along. Incommode he depending do frankness remainder to. Edward day  almost active him friend thirty piqued. People as period twenty my extent as. Set was better abroad ham plenty secure had horses. Admiration has sir decisively excellence say everything inhabiting acceptance.<br> <br>Paid was hill sir high. For him precaution any advantages dissimilar comparison few terminated projecting. Prevailed discovery immediate objection of ye at. Repair summer one winter living feebly pretty his. In so sense am known these since. Shortly respect ask cousins brought add tedious nay.</p>
+                            <br>
+                            <h2>Comments</h2>
+                            <div class="blog-all-comments">
+                                <div class="blog-comments wow fadeInUp" data-wow-delay="0.3s">
+                                    <div class="author-thumb">
+                                        <img src="@/assets/images/commenter1.png" alt="" />
+                                    </div>
+                                    <div class="author-comments">
+                                        <div class="author-details">                                
+                                            <h4>Will Marvin</h4>
+                                            <div class="comment-reply-btn">
+                                                <a href=""><i class="fa fa-share"></i></a>
+                                            </div>
+                                        </div>
+                                        <div class="author-designation">
+                                            1 day ago
+                                        </div>
+                                        <p>Magnis nisl lectus velit parturient vitae, suspendisse fusce in variultusetvitae aenean. Hac metus dui facilisis.</p>
+                                    </div>
                                 </div>
-                                <div class="tab-pane" id="comments">
-                                    <h2>Comments</h2>
-                                    <div class="blog-all-comments">
-                                        <div class="blog-comments wow fadeInUp" data-wow-delay="0.3s">
-                                            <div class="author-thumb">
-                                                <img src="@/assets/images/commenter1.png" alt="" />
-                                            </div>
-                                            <div class="author-comments">
-                                                <div class="author-details">                                
-                                                    <h4>Will Marvin</h4>
-                                                    <div class="comment-reply-btn">
-                                                        <a href=""><i class="fa fa-share"></i></a>
-                                                    </div>
-                                                </div>
-                                                <div class="author-designation">
-                                                    1 day ago
-                                                </div>
-                                                <p>Magnis nisl lectus velit parturient vitae, suspendisse fusce in variultusetvitae aenean. Hac metus dui facilisis.</p>
-                                            </div>
-                                        </div>
-                                        <div class="blog-comments wow fadeInUp" data-wow-delay="0.4s">
-                                            <div class="author-thumb">
-                                                <img src="@/assets/images/commenter2.png" alt="" />
-                                            </div>
-                                            <div class="author-comments">
-                                                <div class="author-details">                                
-                                                    <h4>Mae Hayes</h4>
-                                                    <div class="comment-reply-btn">
-                                                        <a href=""><i class="fa fa-share"></i></a>
-                                                    </div>
-                                                </div>
-                                                <div class="author-designation">
-                                                    1 day ago
-                                                </div>
-                                                <p>Magnis nisl lectus velit parturient vitae, suspendisse fusce in variultusetvitae aenean. Hac metus dui facilisis.</p>
-                                            </div>
-                                        </div>
+                                <div class="blog-comments wow fadeInUp" data-wow-delay="0.4s">
+                                    <div class="author-thumb">
+                                        <img src="@/assets/images/commenter2.png" alt="" />
                                     </div>
-                                    <div class="blog-all-comments mb-60">
-                                        <div class="blog-comments wow fadeInUp" data-wow-delay="0.3s">
-                                            <div class="author-thumb">
-                                                <img src="@/assets/images/commenter3.png" alt="" />
-                                            </div>
-                                            <div class="author-comments">
-                                                <div class="author-details">                                
-                                                    <h4>Taylor Johns</h4>
-                                                    <div class="comment-reply-btn">
-                                                        <a href=""><i class="fa fa-share"></i></a>
-                                                    </div>
-                                                </div>
-                                                <div class="author-designation">
-                                                    1 day ago
-                                                </div>
-                                                <p>Magnis nisl lectus velit parturient vitae, suspendisse fusce in variultusetvitae aenean. Hac metus dui facilisis.</p>
+                                    <div class="author-comments">
+                                        <div class="author-details">                                
+                                            <h4>Mae Hayes</h4>
+                                            <div class="comment-reply-btn">
+                                                <a href=""><i class="fa fa-share"></i></a>
                                             </div>
                                         </div>
-                                        <div class="blog-comments wow fadeInUp" data-wow-delay="0.4s">
-                                            <div class="author-thumb">
-                                                <img src="@/assets/images/commenter2.png" alt="" />
-                                            </div>
-                                            <div class="author-comments">
-                                                <div class="author-details">                                
-                                                    <h4>Mae Hayes</h4>
-                                                    <div class="comment-reply-btn">
-                                                        <a href=""><i class="fa fa-share"></i></a>
-                                                    </div>
-                                                </div>
-                                                <div class="author-designation">
-                                                    1 day ago
-                                                </div>
-                                                <p>Magnis nisl lectus velit parturient vitae, suspendisse fusce in variultusetvitae aenean. Hac metus dui facilisis.</p>
-                                            </div>
+                                        <div class="author-designation">
+                                            1 day ago
                                         </div>
-                                    </div>
-                                    <div class="blog-comment-form wow fadeInUp" data-wow-delay="0.3s">
-                                        <h3>Leave a Comment</h3>
-                                        <form action="#">
-                                            <div class="row">
-                                                <div class="col-md-6">
-                                                    <input type="text" placeholder="Name*" required />
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <input type="email" placeholder="Email*" required />
-                                                </div>
-                                                <div class="col-md-12">
-                                                    <textarea rows="6" placeholder="Message"></textarea>
-                                                </div>
-                                                <div class="col-md-12">
-                                                    <button class="bttn-mid btn-fill" type="submit">Post Comment</button>
-                                                </div>
-                                            </div>
-                                        </form>
+                                        <p>Magnis nisl lectus velit parturient vitae, suspendisse fusce in variultusetvitae aenean. Hac metus dui facilisis.</p>
                                     </div>
                                 </div>
                             </div>
-                            
-                        </div>
+                            <div class="blog-all-comments mb-60">
+                                <div class="blog-comments wow fadeInUp" data-wow-delay="0.3s">
+                                    <div class="author-thumb">
+                                        <img src="@/assets/images/commenter3.png" alt="" />
+                                    </div>
+                                    <div class="author-comments">
+                                        <div class="author-details">                                
+                                            <h4>Taylor Johns</h4>
+                                            <div class="comment-reply-btn">
+                                                <a href=""><i class="fa fa-share"></i></a>
+                                            </div>
+                                        </div>
+                                        <div class="author-designation">
+                                            1 day ago
+                                        </div>
+                                        <p>Magnis nisl lectus velit parturient vitae, suspendisse fusce in variultusetvitae aenean. Hac metus dui facilisis.</p>
+                                    </div>
+                                </div>
+                                <div class="blog-comments wow fadeInUp" data-wow-delay="0.4s">
+                                    <div class="author-thumb">
+                                        <img src="@/assets/images/commenter2.png" alt="" />
+                                    </div>
+                                    <div class="author-comments">
+                                        <div class="author-details">                                
+                                            <h4>Mae Hayes</h4>
+                                            <div class="comment-reply-btn">
+                                                <a href=""><i class="fa fa-share"></i></a>
+                                            </div>
+                                        </div>
+                                        <div class="author-designation">
+                                            1 day ago
+                                        </div>
+                                        <p>Magnis nisl lectus velit parturient vitae, suspendisse fusce in variultusetvitae aenean. Hac metus dui facilisis.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>  
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-5 col-sm-5">
