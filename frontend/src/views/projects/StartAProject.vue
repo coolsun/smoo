dbbc1b3f07b63a4e587e298d1471540ac202af8f
sunb0930 <ben@trillioninnovations.com>
2021-03-08 21:28:44 -0800

    upload image

--- frontend/src/views/projects/StartAProject.vue
+++ frontend/src/views/projects/StartAProject.vue
@@ -15,13 +15,13 @@
                         <form action="#" v-on:submit.prevent>
                             <textarea v-model="projectName" rows="1" :placeholder="$t('campaign.My campaign')"></textarea>
                             <textarea v-model="projectDescription" rows="5" :placeholder="$t('campaign.My campaign is for')"></textarea>
-                            <mdb-select :options="campaignCategory.options" :value.sync="campaignCategory.value" :label="$t('campaign.Choose a category')"
+                            <mdb-select :options="campaignCategory.options" :value="currentCategory" :label="$t('campaign.Choose a category')"
 
                             />
                             <div class="input-group input-group-sm">
-                                <input class="form-control" type="file" @change="onFileChanged" style="height: revert;" />
-                                <button class="btn-sm btn-fill" @click="onUpload">{{ $t('campaign.Upload') }}</button>
+                                <input class="form-control" type="file" @change="onFileChanged" style="height: revert;">        
                             </div>
+                            <img id="preview_image" src="" class="form-control" height="200" alt="Image preview...">
                             <br />
                             <!--div class="upload-img">
                                 <span>Upload campaign image</span>
@@ -75,6 +75,7 @@ export default {
         categoryID: "1",
         projectGoal: "",
         selectedFile: null,
+        selectedFileName: null,
         campaignCategory: {
           value: '1',
           options: [
@@ -87,6 +88,11 @@ export default {
         }
     };
   },
+  computed: {
+      currentCategory () {
+          return this.$store.state.currentCategory;
+      }
+  },
   methods: {
 
     startProject() {
@@ -98,7 +104,9 @@ export default {
             category_id: this.categoryID,
             name: this.projectName,
             description: this.projectDescription,
-            goal: this.projectGoal
+            goal: this.projectGoal,
+            file: this.selectedFile.src,
+            file_name: this.selectedFileName
         }
         this.$axios.post('/api/campaigns', 
             {campaign: newCampaign},
@@ -112,19 +120,23 @@ export default {
             // alert: error
             console.log(error.message)
         }) 
-    }
-  },
-  onFileChanged(event) {
-      this.selectedFile = event.target.files[0];
-  },
-  onFileUpload() {
-    let fileData = {
-        "file": {
-            "name": this.selectedFile.name,
-            "content": this.selectedFile.file
+    },
+    onFileChanged(event) {
+
+        const preview = document.getElementById('preview_image');
+        this.selectedFile = preview;
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.addEventListener("load", function () {
+            // convert image file to base64 string
+            preview.src = reader.result;
+        }, false);
+
+        if (file) {
+            reader.readAsDataURL(file);
+            this.selectedFileName = file.name;
         }
     }
-    this.$axios.post('/campaigns/file-upload', fileData);
   }
 };
 </script>
